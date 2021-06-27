d9215483125522181757849c514e758cdcf8776f
Chathurika Dhananjani <57212615+chathurikadhananjani@users.noreply.github.com>
2021-06-27 23:10:25 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript & react.Js</h1>
+      <p>A basic web dev React JsBootcamp</p>
+    </div>
+  );
+}
+export default Info;
